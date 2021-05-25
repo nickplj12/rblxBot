ea6333e2cbc52ff2d5b90c56e3697155dfd8971c
nickpythonluajava <78268270+nickpythonluajava@users.noreply.github.com>
2021-05-25 14:44:48 -0400

    Update MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -1,1 +1,8 @@
-print("lol nope")
+import time
+print("rblxBot 0.2.1")
+time.sleep(9)
+print("Welcome!")
+time.sleep(3)
+print("User recognized:  rblxai2")
+input('rblxai2@raspbian $ > ')
+print('error')
